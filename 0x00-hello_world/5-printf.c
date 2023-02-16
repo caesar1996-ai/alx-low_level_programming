8073f80d779073bf9d5828b34b5124c32efc1e22
Caesar <caesarnani2015@gmail.com>
2023-02-16 11:18:10 -0800

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main- prints a string followed by a new line
- * Return: 0
+ *main- A program that uses puts to print a string
+ *Return: ends with 0
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("\"Programming is like building a multilingual puzzle");
 return (0);
 }
